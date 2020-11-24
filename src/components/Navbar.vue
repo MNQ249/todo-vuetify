fdbded110334bc6cd31ee497adf49234aa7972b1
Mesfer <mesfer.vip@gmail.com>
2020-11-24 14:33:34 +0300

    create new component navbar.vue,add title + btn, change v-app-toolbar to v-app bar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,21 @@
+<template>
+    <nav>
+        <v-app-bar flat app>
+            <v-toolbar-title class="text-uppercase grey--text">
+                <span class="font-weight-light">Todo</span>
+                <span>Mesfer</span>
+            </v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-btn text color="grey">
+                <span>Sign Out</span>
+                <v-icon>mdi-logout</v-icon>
+            </v-btn>
+        </v-app-bar>
+    </nav>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
