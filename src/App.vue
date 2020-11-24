ef0d972b4f061bb52596b6e7f9743909ed7bd4f8
Mesfer <mesfer.vip@gmail.com>
2020-11-24 14:30:23 +0300

    add navbar, change v-content to v-main, add color grey

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,21 @@
 <template>
-  <v-app>
-    <v-content>
+  <v-app color="grey lighten-4">
+    <Navbar />
+    <v-main>
       <router-view>
       </router-view>
-    </v-content>
+    </v-main>
   </v-app>
 </template>
 
 <script>
-
+import Navbar from '@/components/Navbar'
 
 export default {
   name: 'App',
 
   components: {
-    
+    Navbar,
   },
 
   data: () => ({
