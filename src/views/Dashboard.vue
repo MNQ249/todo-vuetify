f014814ac9ac828ddd0733f831002441cd534dbf
Mesfer <mesfer.vip@gmail.com>
2020-11-25 18:13:18 +0300

    Dummy Project Data

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -4,36 +4,39 @@
 
     <v-container  class="my-5">
 
-    <v-layout row wrap>
+    <v-card flat class="pa-3" v-for="project in projects" :key="project.title">
+      <v-layout row wrap :class="`pa-3 project ${project.status}`">
+        <!-- <v-flex xs12 md6>
+          <v-card-title>Project Title</v-card-title>
+        <v-card-text>
+          <v-row align="center" class="mx-0">
+            Yhoo
+          
+          </v-row>
+        </v-card-text>
+        </v-flex> -->
       <v-flex xs12 md6>
-        <v-btn outlined block color="primary">1</v-btn>
+        <div class="captiion grey--text">Project Title</div>
+        <div>{{ project.title }}</div>
       </v-flex>
 
-      <v-flex xs4 md2>
-        <v-btn outlined block color="primary">2</v-btn>
+      <v-flex xs6 sm4 md2>
+        <div class="captiion grey--text">Person</div>
+        <div>{{ project.person }}</div>
       </v-flex>
 
-       <v-flex xs4 md2>
-        <v-btn outlined block color="primary">2</v-btn>
+      <v-flex xs6 sm4 md2>
+        <div class="captiion grey--text">Due by</div>
+        <div>{{ project.due}}</div>
       </v-flex>
 
-
-      <v-flex xs4 md2>
-        <v-btn outlined block color="primary">2</v-btn>
+      <v-flex xs6 sm4 md2>
+        <div class="captiion grey--text">Status</div>
+        <div>{{ project.status}}</div>
       </v-flex>
 
-    </v-layout>
-
-    <v-layout row wrap justify-space-around>
-      <v-flex xs4 md3>
-        <v-btn outlined block color="success">1</v-btn>
-      </v-flex>
-
-      <v-flex xs4 md3>
-        <v-btn outlined block color="success">2</v-btn>
-      </v-flex>
-    </v-layout>
-
+      </v-layout>
+    </v-card>
    </v-container>
   </div>
 </template>
@@ -43,9 +46,29 @@
 
 
 export default {
-  name: 'Home',
-  components: {
-  
+    data(){
+          return {
+  projects: [
+        { title: 'Design a new website', person: 'The Net Ninja', due: '1st Jan 2019', status: 'ongoing', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+        { title: 'Code up the homepage', person: 'Chun Li', due: '10th Jan 2019', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+        { title: 'Design video thumbnails', person: 'Ryu', due: '20th Dec 2018', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+        { title: 'Create a community forum', person: 'Gouken', due: '20th Oct 2018', status: 'overdue', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+      ]
   }
 }
+}
 </script>
+
+<style>
+ .project.complete {
+   border-left: 4px solid #3cd1c2
+ }
+
+ .project.ongoing{
+   border-left: 4px solid orange;
+ }
+
+ .project.overdue{
+   border-left: 4px solid tomato;
+ }
+</style>
