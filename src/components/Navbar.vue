339612cb318c1a61842ed49999d413561968e51f
Mesfer <mesfer.vip@gmail.com>
2020-11-25 12:32:00 +0300

    create a new list, use property router to connect link

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -13,47 +13,18 @@
             </v-btn>
         </v-app-bar>
 
-        <v-navigation-drawer app v-model="drawer" class="primary"> <!-- v-model conterol of we show or hiden drawer-->
-            <v-list>
-                <v-list-item>
-                    <v-list-item-avatar>
-                        <v-img src="@/assets/circle-cropped.png"></v-img>
-                    </v-list-item-avatar>
-                </v-list-item>
-
-                <v-list-item link>
-                    <v-list-item-content>
-                        <v-list-item-title class="title">Mesfer AlQahtani
-                            </v-list-item-title>
-                            <v-list-item-subtitle>
-                                mesfer.vip@gmail.com
-                            </v-list-item-subtitle>
-                    </v-list-item-content>
-
-                <v-list-item-action>
-                    <v-icon>
-                        mdi-menu-down
-                    </v-icon>
-                </v-list-item-action>
-                </v-list-item>
-
-
-                <v-divider></v-divider>
-                <v-list nav dense></v-list>
-
-                <v-list-item-group v-model="selectedItem" color="primary">
-                    <v-list-item v-for="(item, i) in items" :key="i">
-                        <v-list-item-icon>
-                            <v-icon v-text="item.icon"></v-icon>
-                        </v-list-item-icon>
-                        <v-list-item-content>
-                            <v-list-item-title v-text="item.text"></v-list-item-title>
-                        </v-list-item-content>
-                    </v-list-item>
-
-                </v-list-item-group>
-            </v-list>
-        </v-navigation-drawer>
+       <v-navigation-drawer app v-model="drawer" class="primary">
+           <v-list>
+               <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
+                   <v-list-item-action>
+                       <v-icon class="white--text">{{ link.icon }}</v-icon>
+                   </v-list-item-action>
+                   <v-list-tile-main>
+                       <v-list-tile-title class="white--text">{{ link.text }}</v-list-tile-title>
+                   </v-list-tile-main>
+               </v-list-item>
+           </v-list>
+       </v-navigation-drawer>
     </nav>
 </template>
 
@@ -62,16 +33,12 @@ export default {
     data(){
         return{
             drawer: false,
-            selectedItem: 0,
-    items: [
-      { text: 'My Files', icon: 'mdi-folder' },
-      { text: 'Shared with me', icon: 'mdi-account-multiple' },
-      { text: 'Starred', icon: 'mdi-star' },
-      { text: 'Recent', icon: 'mdi-history' },
-      { text: 'Offline', icon: 'mdi-check-circle' },
-      { text: 'Uploads', icon: 'mdi-upload' },
-      { text: 'Backups', icon: 'mdi-cloud-upload' },
-    ],
+            links:[
+                  {icon: 'mdi-view-dashboard', text: 'Dashboard', route: '/'},
+                  {icon: 'mdi-folder', text: 'My Project', route: '/projects'},
+                  {icon: 'mdi-account-group', text: 'Team', route: '/team'}
+            ]
+    
         }
     }
 }
