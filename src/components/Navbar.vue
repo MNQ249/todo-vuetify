579f5a09421c29204095fa64dd1e244d4f73c51c
Mesfer <mesfer.vip@gmail.com>
2020-11-25 12:53:49 +0300

    change name view(Dashboard,Projects,Team) and setup router for each

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -13,7 +13,7 @@
             </v-btn>
         </v-app-bar>
 
-       <v-navigation-drawer app v-model="drawer" class="primary">
+       <v-navigation-drawer app v-model="drawer" class="accent">
            <v-list>
                <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
                    <v-list-item-action>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <h1>This is Home Page</h1>
+    <h1>Dashboard Page</h1>
   </div>
 </template>
 
