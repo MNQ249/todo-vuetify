ca5ea6d6414e8d21fefd3df731d5e8062c2aa0f7
Mesfer <mesfer.vip@gmail.com>
2020-11-25 16:07:40 +0300

    try using Grid system part1

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -3,7 +3,37 @@
     <h1 class="subtitle grey--text">Dashboard</h1>
 
     <v-container  class="my-5">
-    content
+
+    <v-layout row wrap>
+      <v-flex xs12 md6>
+        <v-btn outlined block color="primary">1</v-btn>
+      </v-flex>
+
+      <v-flex xs4 md2>
+        <v-btn outlined block color="primary">2</v-btn>
+      </v-flex>
+
+       <v-flex xs4 md2>
+        <v-btn outlined block color="primary">2</v-btn>
+      </v-flex>
+
+
+      <v-flex xs4 md2>
+        <v-btn outlined block color="primary">2</v-btn>
+      </v-flex>
+
+    </v-layout>
+
+    <v-layout row wrap justify-space-around>
+      <v-flex xs4 md3>
+        <v-btn outlined block color="success">1</v-btn>
+      </v-flex>
+
+      <v-flex xs4 md3>
+        <v-btn outlined block color="success">2</v-btn>
+      </v-flex>
+    </v-layout>
+
    </v-container>
   </div>
 </template>
