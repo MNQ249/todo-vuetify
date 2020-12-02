801d5cbec4e7081d6f96dd6a5a4c10bbe1f49efe
Mesfer <mesfer.vip@gmail.com>
2020-12-02 12:07:17 +0300

    create new Form basics

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -1,23 +1,56 @@
 <template>
-   <div class="text-center">
-       <v-dialog v-model="dialog" width="500">
-           <template v-slot:activator="{ on, attrs }">
-               <v-btn color="red lighten-1" dark v-bind="attrs" v-on="on" > Add New Project</v-btn>
-           </template>
-           <v-card>
-           <v-card-title class="headline grey lighten-2"> Create a New Project</v-card-title>
-       </v-card>
-       </v-dialog>
-   </div>
+  <div class="text-center">
+    <v-dialog v-model="dialog" width="500">
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn color="red lighten-1" dark v-bind="attrs" v-on="on">
+          Add New Project</v-btn
+        >
+      </template>
+      <v-card>
+        <v-card-title class="headline light-blue accent-4">
+          Create a New Project</v-card-title
+        >
+      </v-card>
+      <v-card-text>
+        <v-form class="px-3">
+          <v-text-field
+            label="Title"
+            v-model="title"
+            prepend-icon="mdi-folder"
+          ></v-text-field>
+          <v-textarea
+            label="Information"
+            v-model="content"
+            prepend-icon="mdi-pencil "
+          ></v-textarea>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+              color="primary"
+              @click="submit"
+            >
+              Upload
+            </v-btn>
+          </v-card-actions>
+        </v-form>
+      </v-card-text>
+    </v-dialog>
+  </div>
 </template>
-
 
 <script>
 export default {
-    data(){
-        return{
-            dialog: true,
-        }
-    }
-}
+  data() {
+    return {
+      dialog: true,
+      title: "",
+      content: "",
+    };
+  },
+  methods:{
+      submit(){
+          console.log(this.title, this.content)
+      }
+  }
+};
 </script>
